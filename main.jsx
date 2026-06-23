943bf8b6444d4c2e0759b2775567dec99e5f0bd7
wac-me <mejbaum@me.com>
2026-06-24 01:57:38 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,10 @@
-main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// Podmień "MojaMakieta" na DOKŁADNĄ nazwę swojego pliku (bez rozszerzenia .jsx)
+import MojaMakieta from './sondal' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MojaMakieta />
+  </React.StrictMode>,
+)
